d69186652a692d067fee52441b701f1cf2fadff0
ozgehalilduman <ozgehalilduman@gmail.com>
2015-04-15 05:45:41 +0300

    Data Annotation kullanımı ve basit hata kontrolu
    Amacım Data Annotation verdiğim alanlardaki hataların nasıl
    yakalandığını göstermek olduğu için  daha fazla Data Annotation şimdilik
    eklemiyorum

---
+++ DERS5/WpfApp_ogrenciOtomasyonu/WpfApp_ogrenciOtomasyonu/ogrenci.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WpfApp_ogrenciOtomasyonu
+{
+    /// <summary>
+    /// ogrencilerin temel propertylerinin-özelliklerinin tanımlandığı kısım
+    /// </summary>
+    public class ogrenci
+    {
+        //GET ve SET kullanımına örnek Veriyorum..
+        //GET degişken degeri okunurken devreye girecek kodların yazıldığı kısım 
+        //SET degişkene değer ataması yapılırken devreye girecek kodların yazıldığı kısım
+        //bugün için get set ayarlamasını ad ve soyad alanlarının dışında kullanmayacağım
+        //ama bu kısmı geliştireceğiz...
+        public int ogrenciID { get; set; }        
+        private string ad;
+        [Required(ErrorMessage = "AD DEGERİ GİRİLMEK ZORUNDA")]
+        public string Ad {
+            get {
+                    if (ad != null)
+                    {
+                        return ad.ToUpper();
+                    }
+                    else
+                    {
+                        return ad;
+                    }                    
+                }//Sınıfın Ad değeri küçük harfle girilmiş olsa bile,okunurken büyük harfe çevrilmesini sağlıyorum
+            set { ad = value; }//Üzerinde kontrol saglayabilmek(işlem yapabilmek) için Ad a girilen degeri private ad a aktarıyorum...
+        }
+        private string soyad;
+        [Required(ErrorMessage = "SOYAD DEGERİ GİRİLMEK ZORUNDA")]
+        public string Soyad {
+            get {
+                    if (soyad != null)
+                    {
+                        return soyad.ToUpper();
+                    }
+                    else
+                    {
+                        return soyad;
+                    }
+            }
+            set { soyad = value; }
+        }
+        [Required(ErrorMessage ="SINIF DEGERİ GİRİLMEK ZORUNDA")]
+        public string Sinif { get; set; }
+        public int Okulno { get; set; }
+        private string tcno;
+        public string TcNo {
+            get {
+                    if (tcno != null)
+                    {
+                        if (tcno.Length == 11)
+                        {
+                            return tcno;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                    
+                }
+            set
+                {
+                    tcno = value;
+                }
+
+        }
+        public string Cinsiyet { get; set; }
+        public byte Boy { get; set; }
+        public byte Kilo { get; set; }
+    }
+}
---
+++ DERS5/WpfApp_ogrenciOtomasyonu/WpfApp_ogrenciOtomasyonu/ogrenci_islemleri.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Validation;//data annotions larda oluşturduğumuz hataları yakalamak için
+
+namespace WpfApp_ogrenciOtomasyonu
+{
+    /// <summary>
+    /// ogrencilerle ilgili işlemlerin eylemlerin tanımlandığı kısım...
+    /// </summary>
+    class ogrenci_islemleri
+    {
+        veriyapisi ogrencikaynak = new veriyapisi();
+        //cinsiyet kısmı için gerekli secenkleri oluşturuyorum
+        ObservableCollection<string> cinsiyetler=new ObservableCollection<string>();
+        //şimdilik baslangıç degerleri ekliyorum... Entity Framework kullanmaya başlayınca sileceğiz      
+        /// <summary>
+        /// YAPILANDIRICI METODUM
+        /// </summary>
+        public ogrenci_islemleri()
+        {
+            ogrencikaynak.ogrenciler.ToList();//local kaynağı dolduruyorum....
+            //beş baslangıc kaydı oluşturuyorum...   
+            
+            if(ogrencikaynak.ogrenciler.Local.Count<1)
+            {
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Ahmet Eren", Soyad = "Peker", Sinif = "TL-11", Okulno = 1100,TcNo="12345678901",Cinsiyet="E",Boy=170,Kilo=70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Mehmet", Soyad = "Acer", Sinif = "TL-11", Okulno = 1102, TcNo = "12345678902", Cinsiyet = "E", Boy = 171, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Arif", Soyad = "Ceren", Sinif = "TL-11", Okulno = 1105, TcNo = "12345678903", Cinsiyet = "E", Boy = 170, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Mert", Soyad = "Güven", Sinif = "TL-11", Okulno = 1106, TcNo = "12345678904", Cinsiyet = "E", Boy = 171, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Yusuf", Soyad = "", Sinif = "TL-11", Okulno = 1107, TcNo = "12345678905", Cinsiyet = "E", Boy = 170, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Mustafa", Soyad = "", Sinif = "TL-11", Okulno = 1108, TcNo = "12345678906", Cinsiyet = "E", Boy = 171, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Kaan", Soyad = "Özgen", Sinif = "TL-11", Okulno = 1131, TcNo = "12345678907", Cinsiyet = "E", Boy = 170, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Emre", Soyad = "Yıldız", Sinif = "TL-11", Okulno = 1134, TcNo = "12345678908", Cinsiyet = "E", Boy = 171, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "Saygın", Soyad = "Bakar", Sinif = "TL-11", Okulno = 1158, TcNo = "12345678909", Cinsiyet = "E", Boy = 170, Kilo = 70 });
+                ogrencikaynak.ogrenciler.Local.Add(new ogrenci() { Ad = "MehmetX", Soyad = "Öz", Sinif = "TL-11", Okulno = 1101, TcNo = "12345678910", Cinsiyet = "E", Boy = 171, Kilo = 70 });
+         
+            }
+            cinsiyetler.Add("E");
+            cinsiyetler.Add("K");
+            vt_guncelle();
+        }
+        ///<summary>
+        /// Tüm Öğrencilerin Listesini Geriye Döndüren Metodumuz 
+        /// </summary>
+        /// <returns>List<ogrenci> mevcut tum ogrencileri listeler</returns>
+        public ObservableCollection<ogrenci> ogrenciListesi()
+        {
+            return ogrencikaynak.ogrenciler.Local;
+        }
+ 
+        public ObservableCollection<string> cinsiyetListesi()
+        {
+            return cinsiyetler;
+        }
+        ///<summary>
+        /// Secilen Öğrenciyi silen Metodumuz 
+        /// </summary>
+        public void ogrenciSil(ogrenci ogr)
+        {
+            ogrencikaynak.ogrenciler.Local.Remove(ogr);
+            vt_guncelle();
+        }
+        ///<summary>
+        /// Secilen Öğrenciyi silen Metodumuz 
+        /// </summary>
+        public void ogrenciSil(List<ogrenci> ogrler)
+        {
+            foreach(ogrenci ogr in ogrler)
+            {
+                ogrencikaynak.ogrenciler.Local.Remove(ogr);
+            }
+            
+            vt_guncelle();
+        }
+        ///<summary>
+        /// Yeni ogrenciyi kaynağa eklemeye yarar 
+        /// </summary>
+        public string ogrenciEkle(ogrenci ogr)
+        {
+            ogrencikaynak.ogrenciler.Local.Add(ogr);
+            string sonuc=vt_guncelle();
+            if (sonuc != "işlem başarılı")
+            {//datagridde gozükmemesi için siliyorum
+                ogrencikaynak.ogrenciler.Local.Remove(ogr);
+            }
+            return sonuc;
+        }
+        public string vt_guncelle()
+        {   //hata yakalama kullanıyorum...
+            try
+            {
+                ogrencikaynak.SaveChanges();
+                return "işlem başarılı";
+            }
+            catch (DbEntityValidationException e)
+            {
+                List<DbValidationError> hatalar=e.EntityValidationErrors.First().ValidationErrors.ToList();
+                string hataMsj="";
+                foreach (DbValidationError hata in hatalar)
+                {
+                    hataMsj+=hata.ErrorMessage+"\n";
+                }
+                return hataMsj;
+            }
+        }
+    }
+}
